40eab132c4daa162e52f638cf77b2481593de26f
oaswap <oaswap@protonmail.com>
2022-01-18 16:08:28 -0600

    fix: Change ether string to rose for add liquidity currency selection

--- src/utils/currencyId.ts
+++ src/utils/currencyId.ts
@@ -1,7 +1,7 @@
 import { Currency, ETHER, Token } from '@oaswap/sdk'
 
 export function currencyId(currency: Currency): string {
-  if (currency === ETHER) return 'BNB'
+  if (currency === ETHER) return 'ROSE'
   if (currency instanceof Token) return currency.address
   throw new Error('invalid currency')
 }
