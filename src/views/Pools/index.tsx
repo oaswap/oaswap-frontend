d85ea77975c00be8b0dd8ebaf153d04a65ef3074
oaswap <oaswap@protonmail.com>
2022-01-31 14:55:25 -0600

    style: Add not live yet notices on farms and pools

--- src/views/Pools/index.tsx
+++ src/views/Pools/index.tsx
@@ -253,7 +253,7 @@ const Pools: React.FC = () => {
         <Flex justifyContent="space-between" flexDirection={['column', null, null, 'row']}>
           <Flex flex="1" flexDirection="column" mr={['8px', 0]}>
             <Heading as="h1" scale="xxl" color="secondary" mb="24px">
-              {t('Oaswap Pools')}
+              {t('Oaswap Pools')} <span style={{ fontSize: 36 }}>(Not Live Yet)</span>
             </Heading>
             <Heading scale="md" color="text">
               {t('Stake OAS tokens to earn.')}
