31def73d306935ab7f02b7c1d37df68f351a2b78
oaswap <oaswap@protonmail.com>
2022-02-01 12:15:19 -0600

    refactor: Change some oas token string symbol names

--- src/components/RoiCalculatorModal/index.tsx
+++ src/components/RoiCalculatorModal/index.tsx
@@ -79,7 +79,7 @@ const RoiCalculatorModal: React.FC<RoiCalculatorModalProps> = ({
   stakingTokenPrice,
   multiplier,
   initialValue,
-  earningTokenSymbol = 'CAKE',
+  earningTokenSymbol = 'OAS',
   autoCompoundFrequency = 0,
   performanceFee = 0,
   isFarm = false,
