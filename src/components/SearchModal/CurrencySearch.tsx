3d5a7dd26286b4b5e4bac6838aa4e9d2a516b93e
oaswap <97272366+oaswap@users.noreply.github.com>
2022-02-22 19:46:20 -0600

    refactor: Add rose token select for currency search input

--- src/components/SearchModal/CurrencySearch.tsx
+++ src/components/SearchModal/CurrencySearch.tsx
@@ -100,7 +100,7 @@ function CurrencySearch({
     (e: KeyboardEvent<HTMLInputElement>) => {
       if (e.key === 'Enter') {
         const s = debouncedQuery.toLowerCase().trim()
-        if (s === 'bnb') {
+        if (s === 'rose') {
           handleCurrencySelect(ETHER)
         } else if (filteredSortedTokens.length > 0) {
           if (
