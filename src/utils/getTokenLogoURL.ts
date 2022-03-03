d00ae0cbfba03cfd7fe70b2635f496aad7a71e7c
John Lander <oaswap@protonmail.com>
2022-03-03 11:57:00 -0600

    refactor: Change trustwallet token logo url to oaswap url

--- src/utils/getTokenLogoURL.ts
+++ src/utils/getTokenLogoURL.ts
@@ -1,4 +1,5 @@
 const getTokenLogoURL = (address: string) =>
-  `https://assets.trustwalletapp.com/blockchains/smartchain/assets/${address}/logo.png`
+  // `https://assets.trustwalletapp.com/blockchains/smartchain/assets/${address}/logo.png`
+  `https://oaswap.finance/images/tokens/${address}.png`
 
 export default getTokenLogoURL
