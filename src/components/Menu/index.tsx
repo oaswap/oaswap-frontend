d9fc736104c064229860a99448faa2346fb1540e
John Lander <oaswap@protonmail.com>
2022-03-08 17:56:50 -0600

    feat: Fetch the rose price from the menu component

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useLocation } from 'react-router'
 import { Menu as UikitMenu } from '@oaswap/uikit'
 import { languageList } from 'config/localization/languages'
@@ -6,6 +6,7 @@ import { useTranslation } from 'contexts/Localization'
 import PhishingWarningBanner from 'components/PhishingWarningBanner'
 import useTheme from 'hooks/useTheme'
 import { usePriceCakeBusd } from 'state/farms/hooks'
+import { getRosePrice } from 'state/farms/fetchFarmsPrices'
 import { usePhishingBannerManager } from 'state/user/hooks'
 import config from './config/config'
 import UserMenu from './UserMenu'
@@ -16,12 +17,26 @@ import { footerLinks } from './config/footerConfig'
 const Menu = (props) => {
   const { isDark, toggleTheme } = useTheme()
   const cakePriceUsd = usePriceCakeBusd()
+  const [rosePriceUsd, setRosePriceUsd] = useState<number>()
   const { currentLanguage, setLanguage, t } = useTranslation()
   const { pathname } = useLocation()
   const [showPhishingWarningBanner] = usePhishingBannerManager()
 
   const activeMenuItem = getActiveMenuItem({ menuConfig: config(t), pathname })
   const activeSubMenuItem = getActiveSubMenuItem({ menuItem: activeMenuItem, pathname })
+
+  // const rosePriceUsd = getRosePrice()
+  // console.log('rosePriceUsd', rosePriceUsd)
+  // console.log(typeof rosePriceUsd)
+
+  useEffect(() => {
+    const fetchRosePrice = async () => {
+      const rosePrice = await getRosePrice()
+      setRosePriceUsd(rosePrice)
+    }
+
+    fetchRosePrice()
+  }, [rosePriceUsd])
 
   return (
     <UikitMenu
@@ -34,6 +49,7 @@ const Menu = (props) => {
       langs={languageList}
       setLang={setLanguage}
       cakePriceUsd={cakePriceUsd.toNumber()}
+      rosePriceUsd={rosePriceUsd}
       links={config(t)}
       subLinks={activeMenuItem?.hideSubNav ? [] : activeMenuItem?.items}
       footerLinks={footerLinks(t)}
